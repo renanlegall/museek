51f795166e0145773ffccd1b9fb501c7977e0d2f
Renan Le Gall <renan.legall@ieseg.fr>
2017-09-05 11:40:16 +0200

    home + fb connect + views

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,21 @@
+class TracksController < ApplicationController
+  def index
+    @tracks = Track.all
+  end
+
+  def show
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
+  get 'tracks/index'
+
+  get 'tracks/show'
+
+  get 'tracks/edit'
+
+  get 'tracks/update'
+
+  get 'tracks/destroy'
+
+  get 'tracks/create'
+
+  root to: 'pages#home'
+
     devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
