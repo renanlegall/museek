2ddabf8183467144f1152aaee37a5735135e9186
Renan Le Gall <renan.legall@ieseg.fr>
2017-09-26 11:56:05 +0200

    some stuff with top playlists

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
 
   def home
     # @disable_footer = true
-    @playlists = Playlist.all
+    @playlists = Playlist.all.sort_by { |playlist| playlist.upvotes.size }.reverse.first(3)
   end
 end
