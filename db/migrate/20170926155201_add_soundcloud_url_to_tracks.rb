c71aa43df40e0550f067ea1edb988bd40db2b8ef
Renan Le Gall <renan.legall@ieseg.fr>
2017-09-27 19:26:40 +0200

    current_user shits

--- db/migrate/20170926155201_add_soundcloud_url_to_tracks.rb
+++
@@ -1,5 +0,0 @@
-class AddSoundcloudUrlToTracks < ActiveRecord::Migration[5.1]
-  def change
-    add_column :tracks, :soundcloud_url, :string
-  end
-end
--- db/migrate/20170926155926_fix_column_name.rb
+++
@@ -1,5 +0,0 @@
-class FixColumnName < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :tracks, :souncloud_url, :soundcloud_url
-  end
-end
