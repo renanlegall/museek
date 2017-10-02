2917a10ee0e3d411443754e1f0dea23504a3cdfe
Renan Le Gall <renan.legall@ieseg.fr>
2017-10-02 12:37:51 +0200

    added production settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_cable.url = "wss://www.museek-app.herokuapp.com/cable"
-config.action_cable.allowed_request_origins = [ "http://museek-app.herokuapp.com", "http://museek-app.herokuapp.com" ]
